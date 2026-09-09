7d387e609e1f9c3da9690ff7f894b1ff820e0b51
lufrz <314570110+lufrz@users.noreply.github.com>
2026-09-09 07:54:54 +0200

    flourite: Add WFD source audiobase compatibility fixup

--- extract-files.py
+++ extract-files.py
@@ -137,6 +137,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/bin/wfdservice64': blob_fixup()
         .add_needed('libwfdservice_shim.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libbinder_shim.so')
