99f41fb8b898005ae1b50c07541de0ac2da558f7
lufrz <314570110+lufrz@users.noreply.github.com>
2026-09-09 09:01:26 +0200

    flourite: Declare proprietary AIDL HALs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,6 +73,7 @@ TARGET_USES_VULKAN := true
 
 # HIDL / VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/hidl/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
