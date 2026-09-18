8cb5797b88f7c89158c88eb902effea62c4096c7
lufrz <314570110+lufrz@users.noreply.github.com>
2026-09-18 14:21:05 +0200

    flourite: Install Xiaomi sensor sub-HAL in hw path
    The sensors multihal only searches vendor and ODM hw directories for sub-HAL plugins. Install a device-local symlink for sensors.xiaomi.v2 so double-tap and screen-off UDFPS sensors can be loaded.

--- device.mk
+++ device.mk
@@ -399,7 +399,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     sensors.dynamic_sensor_hal \
-    sensors.xiaomi.v2
+    sensors.xiaomi.v2 \
+    sensors_xiaomi_v2_hw_symlink
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
