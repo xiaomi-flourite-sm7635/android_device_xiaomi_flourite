18844f92e954da689092fd34841697350526fdc6
lufrz <314570110+lufrz@users.noreply.github.com>
2026-09-18 13:13:01 +0200

    flourite: Enable touchscreen wake sensors
    Package and load the Xiaomi sensors v2 sub-HAL so SystemUI can arm the UDFPS long-press and double-tap wake sensors.

--- device.mk
+++ device.mk
@@ -398,7 +398,8 @@ PRODUCT_PACKAGES += \
     sensor-notifier
 
 PRODUCT_PACKAGES += \
-    sensors.dynamic_sensor_hal
+    sensors.dynamic_sensor_hal \
+    sensors.xiaomi.v2
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
